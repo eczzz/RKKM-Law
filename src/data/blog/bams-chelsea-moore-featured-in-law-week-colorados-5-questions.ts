4cab0b6002327481c1418ebc97423a6d634d1c1b
Stinky Pete <stinkypete@Owlberts-MacBook-Pro.local>
2026-05-19 09:11:26 -0600

    feat: import additional legacy blog posts

---
+++ src/data/blog/bams-chelsea-moore-featured-in-law-week-colorados-5-questions.ts
@@ -0,0 +1,14 @@
+import type { BlogPost } from './types';
+
+export const post: BlogPost = {
+  slug: "bams-chelsea-moore-featured-in-law-week-colorados-5-questions",
+  title: "BAM’s Chelsea Moore Featured in Law Week Colorado’s 5 Questions",
+  category: "Media",
+  featuredImage: "/images/about-denver-aerial.webp",
+  featuredImageAlt: "Denver, Colorado aerial view",
+  excerpt: "BAM Family Law associate attorney Chelsea Moore was featured January 5 in Law Week Colorado’s “5 Questions\" where readers get to know individuals in the legal community a little better.",
+  author: "Kukreja McFarlane Law",
+  publishedDate: "2022-02-18",
+  readTime: "1 min read",
+  body: "<p>BAM Family Law associate attorney Chelsea Moore was featured January 5 in Law Week Colorado’s “5 Questions.” 5Q is a Q&A forum where Law Week’s readers get to know individuals in the legal community a little better. Weekly insights and fun personal facts come from lawyers, paralegals, judges and law professors.</p>\n<p>To see Chelsea’s conversation with Law Week (including her description of the life-size cardboard cutout of Lord of the Rings’ Legolas on her teenage bedroom wall), please <a>click here</a>.</p>",
+};
